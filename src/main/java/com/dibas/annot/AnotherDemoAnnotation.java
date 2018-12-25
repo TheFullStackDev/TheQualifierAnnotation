f6a5cea68a747e1dee1240ba35f8a0db4297fedc
dibasbehera7 <dibasbehera@gmail.com>
2018-12-26 01:04:22 +0530

    Custom Component Scan

---
+++ src/main/java/com/dibas/annot/AnotherDemoAnnotation.java
@@ -0,0 +1,7 @@
+package com.dibas.annot;
+
+import org.springframework.stereotype.Component;
+
+public @interface AnotherDemoAnnotation {
+
+}
---
+++ src/main/java/com/dibas/annot/RandomDemoAnnotation.java
@@ -0,0 +1,8 @@
+package com.dibas.annot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public @interface RandomDemoAnnotation {
+
+}
